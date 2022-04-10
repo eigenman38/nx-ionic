214ed70521c7740a600f409a40a977cb80372c68
eigenman <eigenman38@gmail.com>
2022-04-10 17:10:00 -0600

    created ionic app poker

---
+++ apps/nx-ionic-poker/src/app/home/home.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-ionic-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  constructor() {}
+}
