7e680867afe7e10b81a753a09584fab9eff040ec
eigenman <eigenman38@gmail.com>
2022-04-10 17:33:23 -0600

    moved home page to libs folder

--- libs/mobile/home/feature/src/lib/home.page.ts
+++ libs/mobile/home/feature/src/lib/home.page.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  constructor() {}
+export class HomePageComponent {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() { }
 }
